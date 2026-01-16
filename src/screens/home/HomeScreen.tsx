7949865f357808e746fd193b0a3f8a2d9a7c3858
Gahyun-313 <gus20313@gmail.com>
2026-01-16 22:14:16 +0900

    feat: [ui] 메인 스크린 초기 설정

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { COLORS } from '../../styles/global';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from '../../components/common/Header';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-    </View>
+    <SafeAreaView style={styles.safe} edges={['top', 'bottom']}>
+      {/* Header */}
+      <Header
+        title="실시간 급상승 뉴스"
+        showTimer={true}
+        backgroundColor={COLORS.white}
+      />
+
+      {/* Body */}
+      <View style={styles.container}>
+        <Text>HomeScreen</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: COLORS.white },
+  safe: { flex: 1, backgroundColor: COLORS.white },
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
--- src/screens/mypage/MyPageScreen.tsx
+++ src/screens/mypage/MyPageScreen.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { COLORS } from '../../styles/global';
+import LottieView from 'lottie-react-native';
+import Header from '../../components/common/Header';
 
 export default function MyPageScreen() {
   return (
-    <View style={styles.container}>
-    </View>
+    <SafeAreaView style={styles.safe} edges={['top', 'bottom']}>
+      {/* Header */}
+      <Header
+        title="마이페이지"
+        showTimer={false}
+        backgroundColor={COLORS.white}
+      />
+
+      {/* Body */}
+      <View style={styles.container}>
+        <LottieView
+          source={require('../../assets/lottie/bird.json')}
+          autoPlay
+          loop
+          style={styles.lottie}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: COLORS.white },
+  safe: { flex: 1, backgroundColor: COLORS.white },
+
+  // 간단 헤더(타이틀 중앙)
+  header: {
+    height: 52,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: COLORS.white,
+  },
+  headerTitle: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: '#111827',
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lottie: { width: 200, height: 200 },
 });
