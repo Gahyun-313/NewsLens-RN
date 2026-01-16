a8c382080accdbd8edd6f8f769288551617a3203
Gahyun-313 <gus20313@gmail.com>
2026-01-16 18:10:57 +0900

    chore: [build] 프로젝트 초기 설정

---
+++ src/components/navigation/MainTabs.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../../screens/home/HomeScreen';
+import ForeignScreen from '../../screens/foreign/ForeignScreen';
+import MyPageScreen from '../../screens/mypage/MyPageScreen';
+
+export type MainTabParamList = {
+  Home: undefined;
+  Foreign: undefined;
+  MyPage: undefined;
+};
+
+const Tab = createBottomTabNavigator<MainTabParamList>();
+
+export default function MainTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Foreign" component={ForeignScreen} />
+      <Tab.Screen name="MyPage" component={MyPageScreen} />
+    </Tab.Navigator>
+  );
+}
